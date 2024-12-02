7bc33304a962ec99618a694ea516e6105de71146
Rowddyyy <cimwu00997@ifoxdd.com>
2024-12-02 08:53:56 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=LQFglShI#cb2gtS2vEf2HjMM78zrRtI8CMZzpGr0ED0OeC1ZQeV4",
 };
